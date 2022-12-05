837d62289ee1e5277e67db81cc17743392fbb0f4
hoondal69 <davidtwins6970@gmail.com>
2022-12-05 20:20:48 +0900

    coalesce까지

--- mm.c
+++ mm.c
@@ -44,13 +44,118 @@ team_t team = {
 
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
+/*Define from book*/
+/* Basic constants and macros*/
+#define WSIZE   4
+#define DSIZE   8
+#define CHUNKSIZE   (1<<12)
+
+#define MAX(x,y) ((x) > (y) ? (x): (y))
+
+/*Pack a size and allocated bit into a word*/
+#define PACK(size, alloc)   ((size | alloc))
+
+/*Read and write a word at address p*/
+#define GET(p)       (*(unsigned int *)(p))
+#define PUT(p, val)  (*(unsigned int *)(p) = (val))
+
+/*REad the size and allocated field from address p*/
+#define GET_SIZE(p) (GET(p) & ~0x7)
+#define GET_ALLOC(p) (GET(p) & 0x1)
+
+/*Given block ptr bp, compute address of its header and footer*/
+#define HDRP(bp)    ((char *)(bp) - WSIZE)
+#define FTRP(bp)    ((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE)
+
+/*Given block ptr bp, compute address of next and previous blocks*/
+#define NEXT_BLKP(bp)   ((char *)(bp) + GET_SIZE(((char *)(bp) - WSIZE)))
+#define PREV_BLKP(bp)   ((char *)(bp) - GET_SIZE(((char *)(bp) - DSIZE)))
+
+
+static void *extend_heap(size_t words);
+static void *coalesce(void *bp);
+
 /* 
  * mm_init - initialize the malloc package.
  */
 int mm_init(void)
-{
+{   
+    void *heap_listp;
+    /* Create the initial empty heap */
+    if ((heap_listp = mem_sbrk(4*WSIZE)) == (void*)-1)
+        return -1;
+    PUT(heap_listp, 0);
+    PUT(heap_listp + (1*WSIZE), PACK(DSIZE, 1));
+    PUT(heap_listp + (2*WSIZE), PACK(DSIZE, 1));
+    PUT(heap_listp + (3*WSIZE), PACK(0, 1));
+    heap_listp += (2*WSIZE);
+
+    /* Extend the empty heap with a free block of CHNKSIZE bytes*/
+    if (extend_heap(CHUNKSIZE/WSIZE) == NULL)
+        return -1;
     return 0;
 }
+
+static void *extend_heap(size_t words)
+{
+    char *bp;
+    size_t size;
+
+    /* Allocated an even number of words to maintain alignment*/
+    size = (words % 2) ? (words+1) * WSIZE : words *WSIZE;
+    if ((long)(bp = mem_sbrk(size)) == -1)
+        return NULL;
+    
+    /*Initialize free block header/footer and the epilogue header*/
+    PUT(HDRP(bp), PACK(size, 0));           /*Free block header*/
+    PUT(FTRP(bp), PACK(size, 0));           /*Free block footer*/
+    PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1));   /*New epilogue header*/
+    
+    /*Coalesce if the previous block was free*/
+    return coalesce(bp);
+}
+
+void mm_free(void *bp)
+{
+    size_t size = GET_SIZE(HDRP(bp));
+
+    PUT(HDRP(bp), PACK(size, 0));
+    PUT(FTRP(bp), PACK(size, 0));
+    coalesce(bp);
+}
+
+static void *coalesce(void *bp)
+{
+    size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
+    size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
+    size_t size = GET_SIZE(HERP(bp));
+
+    if (prev_alloc && next_alloc){                  /* Case 1 */
+        return bp;
+    }
+
+    else if (prev_alloc && !next_alloc){            /* Case 2 */
+        size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+        PUT(HDRP(bp), PACK(size, 0));
+        PUT(FTRP(bp), PACK(size, 0));
+    }
+
+    else if (!prev_alloc && next_alloc){            /* Case 3 */
+        size += GET_SIZE(HDRP(PREV_BLKP(bp)));
+        PUT(HDRP(bp), PACK(size, 0));
+        PUT(FTRP(bp), PACK(size, 0));  /*바로 위에 줄에 header를 새로운 size로 할당했으므로, FTRP 를 실행하면 저어어밑에 푸터의 포인터가 나오게 된다.*/
+        bp = PREV_BLKP(bp);
+    }
+
+    else{
+        size += GET_SIZE(HDRP(PREV_BLKP(bp))) + GET_SIZE(FTRP(NEXT_BLKP(bp)));
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+        PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
+        bp = PREV_BLKP(bp);
+    }
+    return bp;
+}
+
 
 /* 
  * mm_malloc - Allocate a block by incrementing the brk pointer.
