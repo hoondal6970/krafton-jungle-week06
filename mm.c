35e9239c4eaa993712595c55a6f645949cbb1fd8
hoondal69 <davidtwins6970@gmail.com>
2022-12-06 15:14:26 +0900

    디버깅전

--- mm.c
+++ mm.c
@@ -74,13 +74,15 @@ team_t team = {
 
 static void *extend_heap(size_t words);
 static void *coalesce(void *bp);
+static void *heap_listp;
+void *mm_malloc(size_t size);
+void *place(char *bp, size_t asize);
 
 /* 
  * mm_init - initialize the malloc package.
  */
 int mm_init(void)
 {   
-    void *heap_listp;
     /* Create the initial empty heap */
     if ((heap_listp = mem_sbrk(4*WSIZE)) == (void*)-1)
         return -1;
@@ -115,6 +117,9 @@ static void *extend_heap(size_t words)
     return coalesce(bp);
 }
 
+/*
+ * mm_free - Freeing a block does nothing.
+ */
 void mm_free(void *bp)
 {
     size_t size = GET_SIZE(HDRP(bp));
@@ -128,7 +133,7 @@ static void *coalesce(void *bp)
 {
     size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
     size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
-    size_t size = GET_SIZE(HERP(bp));
+    size_t size = GET_SIZE(HDRP(bp));
 
     if (prev_alloc && next_alloc){                  /* Case 1 */
         return bp;
@@ -161,23 +166,64 @@ static void *coalesce(void *bp)
  * mm_malloc - Allocate a block by incrementing the brk pointer.
  *     Always allocate a block whose size is a multiple of the alignment.
  */
-void *mm_malloc(size_t size)
-{
-    int newsize = ALIGN(size + SIZE_T_SIZE);
-    void *p = mem_sbrk(newsize);
-    if (p == (void *)-1)
-	return NULL;
-    else {
-        *(size_t *)p = size;
-        return (void *)((char *)p + SIZE_T_SIZE);
+static void *find_fit(size_t asize){            /*first fit 으로 구현하기*/
+    char *bp;
+
+    for(bp = heap_listp; GET_SIZE(HDRP(bp)) == 0; bp = NEXT_BLKP(bp)){
+        if (GET_ALLOC(HDRP(bp)) == 0 && GET_SIZE(HDRP(bp)) >= asize){
+            return (void *) bp;
+        }
     }
+
+    /*for 문 다 돌리면서 return 안됐으면, extend_heap실행하면 된다.*/
+    return NULL;
 }
 
-/*
- * mm_free - Freeing a block does nothing.
- */
-void mm_free(void *ptr)
+void *mm_malloc(size_t size)
 {
+    size_t asize;           /* Adjusted block size */
+    size_t extendsize;      /* Amount to extend geap if no fit */
+    char *bp;
+    
+
+    /* Ignore spurious requests */
+    if (size == 0)
+        return NULL;
+    
+    /* Adjust block size to include overhead and alignment reqs. */
+    if (size <= DSIZE)
+        asize = 2*DSIZE;
+    else
+        asize = DSIZE * ((size + (DSIZE) + (DSIZE -1)) / DSIZE);
+
+    /* Search the free list for a fit */
+    if ((bp = find_fit(asize)) != NULL){
+        place(bp, asize);
+        return bp;
+    }
+
+    /*No fit found. Get more memory and place the block */
+    extendsize = MAX(asize, CHUNKSIZE);
+    if ((bp = extend_heap(extendsize/WSIZE)) == NULL)
+        return NULL;
+    place(bp, asize);
+    return bp;
+}
+
+void *place(char *bp, size_t asize)
+{   
+    size_t csize = GET_SIZE(HDRP(bp));
+
+    if ((csize-asize) >= 2*DSIZE){
+        PUT(FTRP(bp), PACK((csize-asize), 0));
+        PUT(HDRP(bp), PACK(asize, 1));
+        PUT(FTRP(bp), PACK(asize, 1));
+        PUT(HDRP(NEXT_BLKP(bp)), PACK((csize-asize), 0));
+    }
+    else{
+        PUT(HDRP(bp), PACK(csize, 1));
+        PUT(FTRP(bp), PACK(csize, 1));
+    }
 }
 
 /*
